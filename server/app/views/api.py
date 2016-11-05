ed43a1347639ca08e95e5f5d75cdb9f7aef7f991
Tom Marks <thomas.o.marks@gmail.com>
2016-11-05 02:48:53 -0400

    Adds bootstrap

--- server/app/views/api.py
+++ server/app/views/api.py
@@ -35,8 +35,8 @@ def CenusDatum(
 class SampleData(Resource):
     def get(self):
         return [
-            CenusDatum(39.72, -75.545, 15, 3, 1, 1, 1, 1, 1, 1, 1, 1, "foo"),
-            CenusDatum(39.74, -75.544, 15, 3, 1, 1, 1, 1, 1, 1, 1, 1, "foo")
+            CenusDatum(39.72, -75.545, 15, 6, 10, 14, 1, 1, 1, 1, 1, 1, "foo"),
+            CenusDatum(39.74, -75.544, 15, 7, 5, 3, 1, 1, 1, 1, 1, 1, "foo")
         ]
 
 api.add_resource(SampleData, '/')
