94f9c41820e7290da3217399eb3c8389b9169c0c
Tom Marks <thomas.o.marks@gmail.com>
2016-11-05 14:01:37 -0400

    Updates us_census setup.py

--- census/setup.py
+++ census/setup.py
@@ -3,8 +3,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Distutils',
+    name='us_census',
     version='0.0.1dev1',
+    install_requires=[
+        'requests',
+        'census'
+    ],
     description='Wrapper around processed us census data and external apis',
     author='Tom O. Marks & Chris A. Williams',
     author_email='thomas.o.marks@gmail.com',
