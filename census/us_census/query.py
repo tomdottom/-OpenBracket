6d01b8749db44732bf6397f9c0ff3c181aba17a4
Tom Marks <thomas.o.marks@gmail.com>
2016-11-05 10:24:27 -0400

    Add missing 'census' package

--- census/us_census/query.py
+++ census/us_census/query.py
@@ -2,7 +2,7 @@ from types import CenusDatum
 
 
 def query():
-        return [
-            CenusDatum(39.72, -75.545, 15, 6, 10, 14, 1, 1, 1, 1, 1, 1, "foo"),
-            CenusDatum(39.74, -75.544, 15, 7, 5, 3, 1, 1, 1, 1, 1, 1, "foo")
-        ]
+    return [
+        CenusDatum(39.72, -75.545, 15, 6, 10, 14, 1, 1, 1, 1, 1, 1, "foo"),
+        CenusDatum(39.74, -75.544, 15, 7, 5, 3, 1, 1, 1, 1, 1, 1, "foo")
+    ]
