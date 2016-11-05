82f6755bdef036cbe61137111bc816ae4b2d0476
Tom Marks <thomas.o.marks@gmail.com>
2016-11-04 21:54:13 -0400

    Create basic flask app with tutorial leaflet app on homepage

---
+++ server/app/__init__.py
@@ -0,0 +1,10 @@
+# app.py or app/__init__.py
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', name="foo")
